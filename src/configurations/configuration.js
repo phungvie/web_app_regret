d25e40f2f9d4ac919854fd0ea6936118eb90249c
phungvie <phungtienviet14@gmail.com>
2025-05-18 19:15:50 +0700

    first commit

---
+++ src/configurations/configuration.js
@@ -0,0 +1,14 @@
+export const CONFIG = {
+  API_GATEWAY: "http://localhost:8080/profile",
+};
+
+export const API = {
+  REGISTRATION: "/register",
+  MY_PROFILE: "/my-profile",
+};
+
+export const KEYCLOACK_CONFIG = {
+  url: "http://localhost:8090",
+  realm: "regret",
+  clientId: "regret_web_app",
+};
